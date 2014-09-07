6cf44c9540082087aa5006ceb8ecf47bba7a6fb6
sasilukr <p.pandapower@gmail.com>
2014-09-06 22:54:17 -0700

    break into 2 diff js for public

---
+++ public/javascripts/tippy-ninja.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+    getHourRate();
+    $("#simplify-payment-form").on("submit", function() {
+        // Disable the submit button
+        $("#process-payment-btn").attr("disabled", "disabled");
+        // Generate a card token & handle the response
+        SimplifyCommerce.generateToken({
+            key: "sbpb_ZDNiZjZhN2MtOWUzNS00NmI0LTkzYjYtN2YyOTkyZDlkYjFj",
+            card: {
+                number: $("#cc-number").val(),
+                cvc: $("#cc-cvc").val(),
+                expMonth: $("#cc-exp-month").val(),
+                expYear: $("#cc-exp-year").val()
+            }
+        }, simplifyResponseHandler);
+        return false;
+    });
+});
+
+function getHourRate() {
+    var twitterId = $("#tippyNinjaDonate").attr("twitter-id");
+    var url = "/getUserInfo/" + twitterId;
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function(data) {
+            console.log("getUserInfo success: " + JSON.stringify(data));
+            var totalHours = data.totalHours;
+            var totalCash = data.totalCash;
+            var rate = 0;
+            if ( totalCash > 0 ) {
+                rate = (totalCash*100)/totalHours.toFixed(2);
+            }
+            $('#tippyNinjaHourRate').text("$" + rate + " / Hour");
+        },
+        error: function(error) {
+            console.log("getUserInfo error: " + JSON.stringify(error));
+            $('#tippyNinjaHourRate').text("$0 / Hour");
+        }
+    });
+}
+
+function simplifyResponseHandler(data) {
+    console.log("SimplifyResponseHandler " + JSON.stringify(data));
+    var $paymentForm = $("#simplify-payment-form");
+    // Remove all previous errors
+    $(".error").remove();
+    // Check for errors
+    if (data.error) {
+        // Show any validation errors
+        if (data.error.code == "validation") {
+            var fieldErrors = data.error.fieldErrors,
+                fieldErrorsLength = fieldErrors.length,
+                errorList = "";
+            for (var i = 0; i < fieldErrorsLength; i++) {
+                errorList += "<div class='error'>Field: '" + fieldErrors[i].field +
+                    "' is invalid - " + fieldErrors[i].message + "</div>";
+            }
+            // Display the errors
+            $paymentForm.after(errorList);
+        }
+        // Re-enable the submit button
+        $("#process-payment-btn").removeAttr("disabled");
+    } else {
+        // The token contains id, last4, and card type
+        var token = data["id"];
+    }
+}
+
+function processPayment(simplifyToken) {
+    var $paymentForm = $("#simplify-payment-form");
+    var donationAmount = $("#donationAmount").val() * 100;
+    var url = "/processPayment";
+    var twitterId = $("#tippyNinjaDonate").attr("twitter-id");
+    var obj = {
+        'simplifyToken' : simplifyToken,
+        'twitterId'     : twitterId,
+        'amount'        : donationAmount
+    };
+    console.log("Submitting form to our server" + JSON.stringify(obj) );
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: obj,
+        success: function(data) {
+            console.log("processPayment success");
+            alert("Process Payment");
+            $('#paymentModal').modal('hide');
+        },
+        error: function(error) {
+            console.log("processPayment error");
+            $paymentForm.after(error);
+        }
+    });
+}
--- public/javascripts/tippy.js
+++ public/javascripts/tippy.js
@@ -1,118 +1,11 @@
-var twitterId = null;
-$(document).ready(function() {
-    getHourRate();
-    $("#simplify-payment-form").on("submit", function() {
-        // Disable the submit button
-        $("#process-payment-btn").attr("disabled", "disabled");
-        // Generate a card token & handle the response
-        SimplifyCommerce.generateToken({
-            key: "sbpb_ZDNiZjZhN2MtOWUzNS00NmI0LTkzYjYtN2YyOTkyZDlkYjFj",
-            card: {
-                number: $("#cc-number").val(),
-                cvc: $("#cc-cvc").val(),
-                expMonth: $("#cc-exp-month").val(),
-                expYear: $("#cc-exp-year").val()
-            }
-        }, simplifyResponseHandler);
-        return false;
-    });
-});
-
-function getHourRate() {
-    var url = "/getUserInfo/" + twitterId;
-    $.ajax({
-        type: "GET",
-        url: url,
-        success: function(data) {
-            console.log("getUserInfo success: " + JSON.stringify(data));
-            var totalHours = data.totalHours;
-            var totalCash = data.totalCash;
-            var rate = 0;
-            if ( totalCash > 0 ) {
-                rate = (totalCash*100)/totalHours.toFixed(2);
-            }
-            $('#tippyNinjaHourRate').text("$" + rate + " / Hour");
-        },
-        error: function(error) {
-            console.log("getUserInfo error: " + JSON.stringify(error));
-            $('#tippyNinjaHourRate').text("$0 / Hour");
-        }
-    });
-}
-
-function simplifyResponseHandler(data) {
-    console.log("SimplifyResponseHandler " + JSON.stringify(data));
-    var $paymentForm = $("#simplify-payment-form");
-    // Remove all previous errors
-    $(".error").remove();
-    // Check for errors
-    if (data.error) {
-        // Show any validation errors
-        if (data.error.code == "validation") {
-            var fieldErrors = data.error.fieldErrors,
-                fieldErrorsLength = fieldErrors.length,
-                errorList = "";
-            for (var i = 0; i < fieldErrorsLength; i++) {
-                errorList += "<div class='error'>Field: '" + fieldErrors[i].field +
-                    "' is invalid - " + fieldErrors[i].message + "</div>";
-            }
-            // Display the errors
-            $paymentForm.after(errorList);
-        }
-        // Re-enable the submit button
-        $("#process-payment-btn").removeAttr("disabled");
-    } else {
-        // The token contains id, last4, and card type
-        var token = data["id"];
-//        var donationAmount = $("#donationAmount").val() * 100;
-        // Insert the token into the form so it gets submitted to the server
-//        $paymentForm.append("<input type='hidden' name='simplifyToken' value='" + token + "' />");
-//        $paymentForm.append("<input type='hidden' name='twitterId' value='" + twitterId + "' />");
-//        $paymentForm.append("<input type='hidden' name='amount' value='" + donationAmount + "' />");
-
-
-        // Submit the form to the server
-//        $paymentForm.get(0).submit();
-    }
-}
-
-function processPayment(simplifyToken) {
-    var $paymentForm = $("#simplify-payment-form");
-    var donationAmount = $("#donationAmount").val() * 100;
-    var url = "/processPayment";
-    var obj = {
-        'simplifyToken' : simplifyToken,
-        'twitterId'     : twitterId,
-        'amount'        : donationAmount
-    };
-//    console.log("twitterId: " + twitterId);
-//    console.log("simplifyToken: " + token);
-//    console.log("amount: " + donationAmount);
-    console.log("Submitting form to our server" + JSON.stringify(obj) );
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: obj,
-        success: function(data) {
-            console.log("processPayment success");
-            alert("Process Payment");
-            $('#paymentModal').modal('hide');
-        },
-        error: function(error) {
-            console.log("processPayment error");
-            $paymentForm.after(error);
-        }
-    });
-}
-
-
 function signIn() {
     OAuth.initialize('SmThmY15olmUlwfrGWkbdZX7n-M');
     OAuth.popup('twitter').done(function(result) {
         console.log(result);
         result.me().done(function(data) {
             console.log("ME " + JSON.stringify(data));
-            twitterId = data["raw"]["id"];
+            var twitterId = data["raw"]["id"];
+            $("#tippyNinjaDonate").attr("twitter-id", twitterId);
             var url = '/updateHoursWorked';
             var hoursWorked = $("#hoursWorked").val();
             var obj = {
@@ -125,6 +18,8 @@ function signIn() {
                 data: obj,
                 success: function(data) {
                     console.log("updateHoursWorked success");
+                    showCode(twitterId);
+//                    getHourRate();
                 },
                 error: function(error) {
                     console.log("updateHoursWorked error");
@@ -135,5 +30,89 @@ function signIn() {
     });
 }
 
+function showCode(twitterId) {
 
-var codesnippet = '';
+    var tippyNinJaDonateButton = '<script type="text/javascript" src="https://tippyninja.azurewebsites.net/tippy-ninja.js"></script>\
+    <button \
+    id="tippyNinjaHourRate"\
+    class="btn btn"> \
+    </button> \
+    <button\
+        id="tippyNinjaDonate"\
+        twitter-id="' + twitterId + '" ' +
+        'class="btn btn-primary btn"\
+        data-toggle="modal" data-target="#myModal">DONATE\
+    </button>\
+<div class="modal fade" id="paymentModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
+    <div class="modal-dialog">\
+        <div class="modal-content">\
+            <div class="modal-header">\
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>\
+                <h4 class="modal-title" id="myModalLabel">Make donation</h4>\
+            </div>\
+            <form id="simplify-payment-form" action="/processPayment" method="POST">\
+                <div class="modal-body">\
+                    <div class="form-group">\
+                        <label>Donation Amount: </label>\
+                        <input id="donationAmount" type="text"\
+                        class="form-control"\
+                        value="" autofocus />\
+                    </div>\
+                    <div class="form-group">\
+                        <label>Credit Card Number: </label>\
+                        <input id="cc-number" type="text"\
+                        class="form-control"\
+                        maxlength="20" autocomplete="off" value="" autofocus />\
+                    </div>\
+                    <div class="form-group">\
+                        <label>CVC: </label>\
+                        <input id="cc-cvc" type="text"\
+                        class="form-control"\
+                        maxlength="4" autocomplete="off" value=""/>\
+                    </div>\
+                    <div class="form-group ">\
+                        <label>Expiry Date: </label>\
+                        <select id="cc-exp-month" class="form-control">\
+                            <option value="01">Jan</option>\
+                            <option value="02">Feb</option>\
+                            <option value="03">Mar</option>\
+                            <option value="04">Apr</option>\
+                            <option value="05">May</option>\
+                            <option value="06">Jun</option>\
+                            <option value="07">Jul</option>\
+                            <option value="08">Aug</option>\
+                            <option value="09">Sep</option>\
+                            <option value="10">Oct</option>\
+                            <option value="11">Nov</option>\
+                            <option value="12">Dec</option>\
+                        </select>\
+                        <select id="cc-exp-year" class="form-control">\
+                            <option value="13">2013</option>\
+                            <option value="14">2014</option>\
+                            <option value="15">2015</option>\
+                            <option value="16">2016</option>\
+                            <option value="17">2017</option>\
+                            <option value="18">2018</option>\
+                            <option value="19">2019</option>\
+                            <option value="20">2020</option>\
+                            <option value="21">2021</option>\
+                            <option value="22">2022</option>\
+                        </select>\
+                    </div>\
+                </div>\
+                <div class="modal-footer">\
+                    <button\
+                    type="button"\
+                    class="btn btn-default"\
+                    data-dismiss="modal">Close</button>\
+                    <button\
+                    id="process-payment-btn"\
+                    type="submit"\
+                    class="btn btn-primary">Donate</button>\
+                </div>\
+            </form>\
+        </div>\
+    </div>\
+</div>';
+    $("#code").text(tippyNinJaDonateButton);
+}
